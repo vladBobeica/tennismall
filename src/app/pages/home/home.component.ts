d616d3354bd79a90568603fc3478914670bf636d
Vlad Bobeica <vlad.bobeica@outlook.com>
2024-02-28 14:29:57 +0100

    reinit tailwind, add filters

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+  selector: "app-home",
+  templateUrl: "./home.component.html",
 })
 export class HomeComponent {
   cols = 3;
+  category: string | undefined;
 
   onColumnsCountChange(colNum: number): void {
     this.cols = colNum;
   }
 
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+  }
 }
