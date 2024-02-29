bd9497072a16f43f927cc0172720d3478a6471ba
Vlad Bobeica <vlad.bobeica@outlook.com>
2024-02-29 16:40:50 +0100

    add cart logic (add to cart)

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Product } from "../../../../models/product.model";
 
 @Component({
   selector: "app-product-box",
@@ -6,4 +7,15 @@ import { Component, Input } from "@angular/core";
 })
 export class ProductBoxComponent {
   @Input() fullWidthMode = false;
+  product: Product | undefined = {
+    id: 1,
+    title: "NOX",
+    price: 110,
+    category: "racket",
+    image: "https://placehold.co/150",
+  };
+  @Output() addToCart = new EventEmitter();
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
+  }
 }
