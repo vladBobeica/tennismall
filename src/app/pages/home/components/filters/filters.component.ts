e5653e02d81c9cef7c510bdfdff6c19c5e7ad50d
Vlad Bobeica <vlad.bobeica@outlook.com>
2024-03-07 13:11:55 +0100

    add dynamic category selection

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,15 +1,39 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from "@angular/core";
+import { StoreService } from "../../../../services/store.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-filters",
   templateUrl: "./filters.component.html",
 })
-export class FiltersComponent {
+export class FiltersComponent implements OnInit, OnDestroy {
   @Output() showCategory = new EventEmitter<string>();
+  categoriesSubsription: Subscription | undefined;
+  categories: Array<string> | undefined;
 
-  categories = ["rackets", "shoes"];
+  constructor(private storeService: StoreService) {}
+
+  ngOnInit(): void {
+    this.categoriesSubsription = this.storeService
+      .getAllCategories()
+      .subscribe((response) => {
+        this.categories = response;
+      });
+  }
 
   onShowCategory(category: string): void {
     this.showCategory.emit(category);
+  }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubsription) {
+      this.categoriesSubsription.unsubscribe();
+    }
   }
 }
